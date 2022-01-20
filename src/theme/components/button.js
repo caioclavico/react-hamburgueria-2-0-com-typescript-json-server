063efba6d7705b33e4867bacd8771e67fb90f948
Caio Henrique Clavico Crizanto <caiohclavico@gmail.com>
2022-01-20 18:06:50 -0400

    feature: created theme with chakra-ui

---
+++ src/theme/components/button.js
@@ -0,0 +1,45 @@
+export const Button = {
+  baseStyle: {
+    fontFamily: "Inter",
+    fontWeight: "semibold",
+    borderRadius: "8px",
+    padding: "0px 20px",
+    fontSize: "16px",
+  },
+  sizes: {
+    md: {
+      height: "40px",
+    },
+    lg: {
+      height: "60px",
+    },
+  },
+  variants: {
+    default: {
+      border: "2px solid",
+      borderColor: "primary.main",
+      bg: "primary.main",
+      color: "white",
+      _hover: {
+        bg: "#93D7AF",
+        color: "#FFFFFF",
+        borderColor: "#93D7AF",
+      },
+    },
+    disable: {
+      border: "2px solid",
+      borderColor: "gray.100",
+      bg: "gray.100",
+      color: "gray.300",
+      _hover: {
+        bg: "gray.300",
+        color: "gray.100",
+        borderColor: "gray.300",
+      },
+    },
+  },
+  defaultProps: {
+    size: "md",
+    variant: "default",
+  },
+};
---
+++ src/theme/index.js
@@ -0,0 +1,78 @@
+import { extendTheme } from "@chakra-ui/react";
+import { Button } from "./components/button";
+
+const theme = extendTheme({
+  colors: {
+    primary: {
+      main: "#27AE60",
+    },
+    secondary: {
+      main: "#EB5757",
+    },
+    gray: {
+      0: "#f5f5f5",
+      100: "#e0e0e0",
+      300: "#828282",
+      600: "#333333",
+    },
+    negative: {
+      main: "#e60000",
+    },
+    warning: {
+      main: "#ffcd07",
+    },
+    success: {
+      main: "#168821",
+    },
+    information: {
+      main: "#155BCB",
+    },
+  },
+  textStyles: {
+    h1: {
+      fontSize: "26px",
+      fontWeight: "bold",
+    },
+    h2: {
+      fontSize: "22px",
+      fontWeight: "bold",
+    },
+    h3: {
+      fontSize: "18px",
+      fontWeight: "bold",
+    },
+    headline: {
+      fontSize: "16px",
+      fontWeight: "regular",
+    },
+    body: {
+      fontSize: "14px",
+      fontWeight: "regular",
+    },
+    "body-600": {
+      fontSize: "14px",
+      fontWeight: "semibold",
+    },
+    caption: {
+      fontSize: "12px",
+      fontWeight: "regular",
+    },
+  },
+  fonts: {
+    heading: "Inter",
+    body: "Inter",
+  },
+  styles: {
+    global: {
+      body: {
+        bg: "white",
+        color: "gray.600",
+      },
+    },
+  },
+  components: {
+    Button,
+  },
+});
+
+export default theme;
